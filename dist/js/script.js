528362ced10d7d55c3e9fed3b7224c91c059862d
Vlad1235 <fn1235@bk.ru>
2020-06-22 11:26:25 +0300

    desktop version

---
+++ dist/js/script.js
@@ -0,0 +1,136 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+ 
+
+    $('.carousel__inner').slick({
+        speed: 300,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/page1/block4/block4_leftarrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/page1/block4/block4_rightarrow.png"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }]
+    });
+
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.catalog__wrapper').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (event) {
+                event.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    }
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+
+
+    // activate Modal windows
+
+    $("[data-modal=consultation]").on('click', function () {
+        $('.overlay, #consultation').fadeIn('fast');
+    });
+
+    $(".button_mini").each(function (i) {
+        $(this).on('click', function () {
+            $("#order .modal__descr").text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('fast');
+        });
+    });
+
+    $(".modal__close").on("click", function () {
+        $('.overlay, #consultation, #thanks, #order').fadeOut("fast");
+    });
+
+
+    // validation
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символов!")
+                },
+                phone: "Пожалуйста, введите телефон",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    }
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+
+    // mask for input for form
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    // submit data to server and relay it to chosen email
+
+    $('form').submit(function(event) {
+        event.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "../mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find('input').val('');
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    // scroll and page up
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 900) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+    
+    $("a[href=#up]").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    // animation
+   
+    new WOW().init();
+    
+    
+
+
+});
